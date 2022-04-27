353802b6e5bfbb1d35c77f1987948bf4f31ab4c6
sweetzachary <a.ntigr16@gmail.com>
2022-04-27 14:26:10 +0100

    Add path_correct function

--- knights_travails.rb
+++ knights_travails.rb
@@ -4,8 +4,12 @@ def knight_moves(startpos, endpos)
   queue.unshift(starting_path)
   loop do
     path = queue.pop
-    return path if path_correct(path, endpos)
+    return path if path_correct?(path, endpos)
 
     possible_continuations(path).each { |c| queue.unshift c }
   end
+end
+
+def path_correct?(path, endpos)
+  path.last == endpos
 end
