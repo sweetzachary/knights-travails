401890c0cfbe96ea351127dfe01dfca177fd72e0
sweetzachary <a.ntigr16@gmail.com>
2022-04-27 14:56:45 +0100

    Add possible_continuations(path) function

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,3 +1,5 @@
+require 'matrix'
+
 def knight_moves(startpos, endpos)
   starting_path = [startpos]
   queue = []
@@ -12,4 +14,29 @@ end
 
 def path_correct?(path, endpos)
   path.last == endpos
+end
+
+def possible_continuations(path)
+  move_vectors = [[1, 2],
+                  [2, 1],
+                  [2, -1],
+                  [1, -2],
+                  [-1, -2],
+                  [-2, -1],
+                  [-2, 1],
+                  [-1, 2]]
+  lastpos = path.last
+  next_positions = move_vectors.map { |v| sum(v, lastpos) }
+  next_positions = next_positions.filter { |p| position_allowed? p}
+  next_positions.map { |p| path + [p] }
+end
+
+def sum(a1, a2)
+  v1 = Vector.elements(a1)
+  v2 = Vector.elements(a2)
+  (v1 + v2).to_a
+end
+
+def position_allowed?(pos)
+  pos[0].between? 0, 7 && pos[1].between? 0, 7
 end
